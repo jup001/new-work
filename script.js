80c29bdecf97006a871d294bffb05ee6c7d25aa8
jup001 <juprub@gmail.com>
2023-02-11 17:32:28 +0400

    main

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ btn.addEventListener("mouseout", () => {
 sel.addEventListener("mouseout", () => {
     hiden[0].classList.toggle("hiden")
 })
-
+const hide = document.getElementById("hide")
+const btn2 = document.getElementById("btn2")
+const textpage = document.getElementById("textpage")
+btn2.addEventListener("click", () => {
+    hide.classList.toggle("none")
+})
+btn2.addEventListener("click", () => {
+    textpage.classList.toggle("textpage")
+})
