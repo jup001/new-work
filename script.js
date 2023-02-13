1a751c41878d12ed970369a9f174d93dff535b11
jup001 <juprub@gmail.com>
2023-02-14 01:10:24 +0400

    first commit

--- script.js
+++ script.js
@@ -17,8 +17,46 @@ const hide = document.getElementById("hide")
 const btn2 = document.getElementById("btn2")
 const textpage = document.getElementById("textpage")
 btn2.addEventListener("click", () => {
-    hide.classList.toggle("none")
+    hide.classList.add("none")
 })
 btn2.addEventListener("click", () => {
     textpage.classList.toggle("textpage")
+})
+btn2.addEventListener("click", () => {
+    search.classList.add("search")
+})
+btn2.addEventListener("click", () => {
+    side.classList.remove("sidebar2")
+})
+btn2.addEventListener("click", () => {
+    main.classList.remove("main2")
+})
+btn2.addEventListener("click", () => {
+    home.classList.remove("home2")
+})
+const home = document.getElementById("home2")
+const btn3 = document.getElementById("btn3")
+const side = document.getElementById("sidebar")
+const mail = document.getElementById("main")
+const search = document.getElementById("search")
+btn3.addEventListener("click", () => {
+    home.classList.add("home2")
+})
+btn3.addEventListener("click", () => {
+    hide.classList.add("none")
+})
+btn3.addEventListener("click", () => {
+    side.classList.add("sidebar2")
+})
+btn3.addEventListener("click", () => {
+    main.classList.add("main2")
+})
+btn3.addEventListener("click", () => {
+    search.classList.remove("search")
+})
+btn3.addEventListener("click", () => {
+    hide.classList.add("none")
+})
+btn3.addEventListener("click", () => {
+    textpage.classList.add("textpage")
 })
